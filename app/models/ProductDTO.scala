5329849554332648a93a7bb0bf21a54f790b7dd7
michalharasim <96840425+michalharasim@users.noreply.github.com>
2025-03-23 23:36:43 +0100

    Delete app directory

--- app/models/ProductDTO.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-import play.api.libs.json._
-
-case class ProductDTO(name: String, price: Double)
-
-object ProductDTO {
-  implicit val format: OFormat[ProductDTO] = Json.format[ProductDTO]
-}
